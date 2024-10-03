d5a78a404c30f81dbdfcf31cdc2533ef83f7cf4b
anurag438 <davidanurag6@gmail.com>
2024-10-04 01:03:50 +0530

    pug with layouts updated

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
+const path = require('path');
 
 const express = require('express');
-const bodyParser=require('body-parser')
-const app = express()
+const bodyParser = require('body-parser');
 
-app.set('view engine','pug');
+const app = express();
+
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
-const path = require('path')
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const adminData= require('./routes/admin');
-const shopRoutes= require('./routes/shop');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
-app.use("/admin",adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    console.log(req.url)
-    // res.status(404).send(`<h1>Page not found</h1>`)
-    // res.status(404).send(`<h1>Page - ${req.url} not found</h1>`)
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
-
-// module.exports = path.dirname(process.mainModule.filename);
-// --use--
-// module.exports = path.dirname(require.main.filename);
